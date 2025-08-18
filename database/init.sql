35249e7e3d7872def8c1c50fcec25d57da04325e
Valentain <isaev.valentint2022@yandex.ru>
2025-08-18 20:40:23 +0300

    Write start postgres server

--- database/init.sql
+++ database/init.sql
@@ -1,1 +1,9 @@
-\dt
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = :'user_name') THEN
+        CREATE USER :'user_name' WITH PASSWORD :'user_password';
+    END IF;
+END
+$$;
+
+GRANT ALL PRIVILEGES ON DATABASE :'db_name' TO :'user_name';
